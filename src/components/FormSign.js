a241cec7b3dbee44ac6f71b48dcb502e7752b97d
Andrey Kirpichenok <a.kirpichenok99@gmail.com>
2022-05-02 23:58:21 +0300

    rewrite custom hook

--- src/components/FormSign.js
+++ src/components/FormSign.js
@@ -1,4 +1,3 @@
-import { useState } from "react"
 import { useDispatch } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import useValidate from "../hooks/useValidate"
@@ -10,57 +9,48 @@ import PropTypes from 'prop-types'
 const FormSign = ({ type, fromPage }) => {
 
     const dispatch = useDispatch()
-    const { validation } = useValidate()
+    const { validation, isUserValid, isPasswordValid } = useValidate()
     const navigate = useNavigate()
 
-    const [valid,setValid] = useState({
-        isUserValid: true,
-        isPasswordValid: true
-    })
 
     const handleSubmit = e => {
         e.preventDefault()
         const form = e.target;
         const user = form.username.value
         const password = form.password.value
-        if(validation(user,'login') && validation(password,'password')){
-        type === 'Login' ? dispatch(signin({user,password})) : dispatch(signup({user,password}))
-        navigate(fromPage, {replace:true})
-    }else{
-        setValid({
-            isUserValid: validation(user,'login'),
-            isPasswordValid: validation(password, 'password')
-        })
-    }
+        if (validation(user, 'login') && validation(password, 'password')) {
+            type === 'Login' ? dispatch(signin({ user, password })) : dispatch(signup({ user, password }))
+            navigate(fromPage, { replace: true })
+        }
     }
 
     return <>
-    <h1>{type} Page</h1>
-     <form onSubmit={handleSubmit} noValidate>
-                <label htmlFor='email'>Name:</label>
-                     <input 
-                     type='email' 
-                     id='email' 
-                     name='username' 
-                     required
-                     />
-                {!valid.isUserValid && <p className="invalid">username is invalid</p>}
-                <label htmlFor='password'>Password:</label>
-                     <input 
-                     type='password' 
-                     id='password' 
-                     name='password' 
-                     required
-                     />
-                <button type="submit">Login</button>
-            </form>
-            
-            {!valid.isPasswordValid && <p className="invalid">password is invalid</p>}
-            </>
+        <h1>{type} Page</h1>
+        <form onSubmit={handleSubmit} noValidate>
+            <label htmlFor='email'>Name:</label>
+            <input
+                type='email'
+                id='email'
+                name='username'
+                required
+            />
+            {!isUserValid && <p className="invalid">username is invalid</p>}
+            <label htmlFor='password'>Password:</label>
+            <input
+                type='password'
+                id='password'
+                name='password'
+                required
+            />
+            <button type="submit">Login</button>
+        </form>
+
+        {!isPasswordValid && <p className="invalid">password is invalid</p>}
+    </>
 }
 
 FormSign.propTypes = {
-    type: PropTypes.oneOf(['Login','registration']),
+    type: PropTypes.oneOf(['Login', 'registration']),
     fromPage: PropTypes.string
 }
 
