7d615b124297664f0b53482de7a57c4d674ed6f0
Andrey Kirpichenok <a.kirpichenok99@gmail.com>
2022-05-02 20:50:54 +0300

    fix code

--- src/pages/favourites/Favourites.js
+++ src/pages/favourites/Favourites.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import { Characters } from "../searching/output/characters/Characters"
 import { Deaths } from "../searching/output/deaths/Deaths"
@@ -8,7 +8,7 @@ import { Quotes } from "../searching/output/quotes/Quotes"
 export const Favourites = () => {
 
     const { favourites } = useSelector(state => state.favourites)
-    const [nonEmptyFavourites] = useState(Object.entries(favourites))
+    const [nonEmptyFavourites,setFavourites] = useState(Object.entries(favourites))
 
     const showFavourites = (item) => {
        if (item[0] === 'characters') return <div><Characters cards={Object.values(item[1])} /></div>
@@ -16,6 +16,9 @@ export const Favourites = () => {
        if (item[0] === 'quotes') return <div><Quotes cards={Object.values(item[1])} /></div>
        if (item[0] === 'episodes') return <div><Episodes cards={Object.values(item[1])} /></div>
     }
+    useEffect(() => {
+        setFavourites(Object.entries(favourites))
+    },[favourites])
         
     return (
         <div className="favourites">
